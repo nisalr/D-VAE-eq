fd6cb41bfc032e0760a8bd5d81b881ff54878eef
Nisal Ranasinghe <nsranasinghe@student.unimelb.edu.au>
2023-05-02 10:18:42 +1000

    slurm files

--- scripts/bo_experiments.sh
+++ scripts/bo_experiments.sh
@@ -21,6 +21,9 @@
 # The maximum running time of the job in days-hours:mins:sec
 #SBATCH --time=02-00:00:0
 
+# job array
+#SBATCH --array=0-0
+
 # Send yourself an email when the job:
 #SBATCH --mail-user=nsranasinghe@student.unimelb.edu.au
 #SBATCH --mail-type=BEGIN,FAIL,END
@@ -32,12 +35,14 @@ conda activate dvae
 #Run program
 cd ..
 python bayesian_optimization/bo.py \
-  --data-name eq_structures_3 \
-  --save-appendix DVAE_EQ_120K_200epoch \
-  --checkpoint 200 \
-  --res-dir="EQ_results_12/" \
-  --BO-rounds 10 \
-  --BO-batch-size 200 \
+  --data-name eq_structures_7_w_vals \
+  --save-appendix DVAE_EQ_83K_w_vals_200epoch \
+  --checkpoint 300 \
+  --res-dir="EQ_results/EQ_results_25_${SLURM_ARRAY_TASK_ID}/" \
+  --BO-rounds 1 \
+  --BO-batch-size 5 \
   --random-as-test \
   --random-as-train \
   --random-baseline \
+  --dnum ${SLURM_ARRAY_TASK_ID} \
+  --cond
