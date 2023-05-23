0b51520b3c163d6901c598054bc88414215751cf
Nisal Ranasinghe <nsranasinghe@student.unimelb.edu.au>
2023-05-23 16:14:13 +1000

    parameter for condition size

--- models.py
+++ models.py
@@ -7,7 +7,6 @@ import torch.nn.init as init
 import numpy as np
 import igraph
 import pdb
-from src.nesymres import model
 
 # This file implements several VAE models for DAGs, including SVAE, GraphRNN, DVAE, GCN etc.
 
